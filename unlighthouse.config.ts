8dfed41e5355c0700a610c9b4f3a30897579c38e
Avendaño <delacruzavendanohenri@gmail.com>
2025-11-13 11:04:55 -0500

    arreglando errores

--- unlighthouse.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineUnlighthouseConfig } from 'unlighthouse/config'
-
-export default defineUnlighthouseConfig({
-  site: 'https://www.casagrandegeotecnia.com.pe',
-  scanner: {
-    device: 'desktop',   // 👈 fuerza escritorio SIEMPRE
-    throttle: false,
-  },
-})
